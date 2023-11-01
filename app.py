e9e1d648659bf53e2859774003e9eab870e47cac
Elias Achi <elias@plazacybersecurity.com>
2023-11-01 19:46:13 -0400

    Fix

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def contact():
     # first, check recaptcha
     captcha_valid = verify_captcha(gctoken, recaptcha_key)
     if not captcha_valid:
-        return make_response(jsonify({'data': 'Captcha failed', 'error': 'Captcha failed' }), 404)
+        return jsonify({'data': 'Captcha failed', 'error': 'Captcha failed' }), 404
 
     msg = MIMEMultipart('alternative')
     msg['From'] = sender_email
@@ -113,10 +113,10 @@ def contact():
         server = smtplib.SMTP(sender_host, int(sender_port))
         server.sendmail(sender_email, receiver_email, msg.as_string())
         server.quit()
-        return make_response(jsonify({'data': 'Success'}), 200)
+        return jsonify({'data': 'Success'}), 200
     except Exception as e:
         app_logger.warning(e)
-        return make_response(jsonify({'error': f'Error: {e}', 'data': f'Error: {e}'}), 500)
+        return jsonify({'error': f'Error: {e}', 'data': f'Error: {e}'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True, port=3001, host='0.0.0.0')
